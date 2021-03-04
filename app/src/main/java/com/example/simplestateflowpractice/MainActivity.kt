7e811a4243f9448e22c11a3e0676089ccafb1b79
Amirreza.dq <amirreza.dq@gmail.com>
2021-03-04 21:32:23 +0330

    Setup Simple Login :)

--- app/src/main/java/com/example/simplestateflowpractice/MainActivity.kt
+++ app/src/main/java/com/example/simplestateflowpractice/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.simplestateflowpractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import com.example.simplestateflowpractice.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
 
 @ExperimentalCoroutinesApi
 class MainActivity : AppCompatActivity() {
@@ -18,6 +22,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            viewModel.loginUser(
+                binding.etUsername.text.toString(),
+                binding.etPassword.text.toString()
+            )
+        }
+
+        lifecycleScope.launchWhenCreated {
+            viewModel.loginUiState.collect {
+                when (it) {
+                    is LoginUiState.Success -> {
+
+                        Snackbar.make(
+                            binding.root,
+                            "Successfully logged in!",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+
+                        binding.progressBar.isVisible = false
+                    }
+                    is LoginUiState.Error -> {
+
+                        Snackbar.make(
+                            binding.root,
+                            it.message,
+                            Snackbar.LENGTH_LONG
+                        ).show()
+
+                        binding.progressBar.isVisible = false
+
+                    }
+                    is LoginUiState.Loading ->{
+
+                        binding.progressBar.isVisible = true
+
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
     }
 
     override fun onDestroy() {
